91be10d748b7ccd9b8f037f6bcfe20f78bdfafe1
Jeison <jeison-noctiz@hotmail.com>
2017-04-23 18:12:11 -0500

    mapa demo juego

---
+++ juego/Assets/scripts/moverclick.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moverclick : MonoBehaviour {
+
+    private Vector3 _target;
+    public bool _hasArrive;
+    public float _speed;
+    public float _distanceToArrive;
+    public GameObject particle;
+    RaycastHit hit;
+    // Use this for initialization
+    void Start()
+    {
+        begin();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        move();          
+        if (Input.GetMouseButtonDown(0)){
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                
+                _target = hit.point;
+            }
+            Debug.Log(_target);                          
+        }
+       
+        
+    }
+    
+    private void Arrive()
+    {
+         begin();
+    }
+
+   
+
+    public bool HasArrive()
+    {
+        if ((transform.position - _target).magnitude <= _distanceToArrive)
+            return true;
+        return false;
+    }
+
+    private void begin()
+    {
+        
+        _hasArrive = false;
+
+    }
+
+    public void move()
+    {
+        
+            Vector3 direction = _target - this.transform.position;
+            Vector3 step = direction.normalized * _speed * Time.deltaTime;
+            this.transform.position += step;
+            _hasArrive = HasArrive();           
+        
+
+    }
+}
